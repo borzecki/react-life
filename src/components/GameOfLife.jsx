8451fec294e00a361cbf656456efa76ca38adf99
Daniel <borzecki.daniel@gmail.com>
2020-06-21 12:35:21 +0200

    toggle action for some fine pixel art

--- src/components/GameOfLife.jsx
+++ src/components/GameOfLife.jsx
@@ -12,18 +12,21 @@ const Cell = styled.div`
   border: 1px solid #0003;
   height: 4rem;
   margin: 0.1rem;
+
+  ${({ isActive }) => isActive && `background: green;`};
 `;
 
 const GameOfLife = () => {
   const dispatch = useDispatch();
-  const rows = useSelector((state) => state.rows);
-  const cols = useSelector((state) => state.cols);
+  const { rows, cols, active } = useSelector((state) => state);
+
   return (
     <Wrapper cols={cols}>
       {[...Array(rows * cols).keys()].map((i) => (
         <Cell
           key={i}
-          onClick={() => dispatch({ type: "resize", rows: i + 1, cols: 20 })}
+          isActive={active.indexOf(i) > -1}
+          onClick={() => dispatch({ type: "toggle", id: i })}
         />
       ))}
     </Wrapper>
--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,15 @@
 const initialState = { rows: 10, cols: 20, active: [] };
 
+const isActive = (id, list) => list.indexOf(id) > -1;
+const toggleItem = (id, list) =>
+  isActive(id, list) ? list.filter((k) => k !== id) : [id, ...list];
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "resize":
       return { ...state, rows: action.rows, cols: action.cols };
+    case "toggle":
+      return { ...state, active: toggleItem(action.id, state.active) };
     default:
       return state;
   }
