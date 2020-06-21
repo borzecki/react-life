7ee16f80387ad8102171c63a6d07961fa3903dac
Daniel <borzecki.daniel@gmail.com>
2020-06-21 16:57:07 +0200

    Updates

---
+++ precache-manifest.6f8c5dbaedf1006c5d069b0b59f1e3d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052c663474c48271e4ef4eab97b1238e",
+    "url": "/react-life/index.html"
+  },
+  {
+    "revision": "eb0f12fc9977954e415f",
+    "url": "/react-life/static/js/2.b255e2c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-life/static/js/2.b255e2c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "390a1fc71357271b9fa0",
+    "url": "/react-life/static/js/main.8c563944.chunk.js"
+  },
+  {
+    "revision": "d1da2c0c44b7b2bfcf27",
+    "url": "/react-life/static/js/runtime-main.f158dd1e.js"
+  }
+]);
